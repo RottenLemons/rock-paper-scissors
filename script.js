b1eafc5f2d4c6ce5062795861180ab902bba352b
RottenLemons <rottenlemons123@gmail.com>
2022-11-29 10:46:43 +0800

    Add UI

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 let wordToInt = ['Rock', 'Paper', 'Scissors']; // Converts word to number, with the number being the index
-let userScore; 
-let comScore;
+let userScore = 0; 
+let comScore = 0;
 
-function getUserInput() {
-    let userInput = prompt("Enter Rock, Paper or Scissors"); // Gets input from user
-    userInput = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase(); // Convert the input to the form seen in wordToInt
-    return wordToInt.indexOf(userInput); // Returns index of the input
+const buttons = Array.from(document.querySelectorAll(".buttons button"));
+const result = document.querySelector('.result');
+const winner = document.querySelector('.winner');
+const score = document.querySelector('.score');
+
+buttons.forEach((button) => {
+    button.addEventListener('click' ,(e) => {startRound(e);});
+});
+
+function startRound(e) {
+    let userInput = e.target.classList[0][0].toUpperCase() +  e.target.classList[0].slice(1).toLowerCase();
+    let comInput = Math.floor(Math.random() * 3);
+    getResult(wordToInt.indexOf(userInput), comInput);
 }
 
-function getWinner(userInput, comInput) {
+function getResult(userInput, comInput) {
     let change = comInput - userInput; // Gets diff in the index of the inputs
     switch (change) {
         case -1: // If it is -1, it suggests that the index of userInput is after that of comInput, meaning you win
             userScore++;
-            return `You Win! ${wordToInt[userInput]} beats ${wordToInt[comInput]}`;
+            result.textContent = `You Win! ${wordToInt[userInput]} beats ${wordToInt[comInput]}`;
+            break;
         case -2: // If it is -1, it suggests that you are Scissors, while the com is Rock as they are 2 and 0 respectively
             comScore++;
-            return `You Lose! ${wordToInt[comInput]} beats ${wordToInt[userInput]}`;
+            result.textContent = `You Lose! ${wordToInt[comInput]} beats ${wordToInt[userInput]}`;
+            break;
         case 1: // Same case as before but reversed
             comScore++;
-            return `You Lose! ${wordToInt[comInput]} beats ${wordToInt[userInput]}`;            
+            result.textContent = `You Lose! ${wordToInt[comInput]} beats ${wordToInt[userInput]}`;   
+            break;         
         case 2: // Same case as before but reversed
             userScore++;
-            return `You Win! ${wordToInt[userInput]} beats ${wordToInt[comInput]}`;
+            result.textContent = `You Win! ${wordToInt[userInput]} beats ${wordToInt[comInput]}`;
+            break;
         default: // If it is 0, both indexes were the same
-            return "It's a Tie!";
+            result.textContent = "It's a Tie!";
     }
+    score.textContent = `${userScore} - ${comScore}`;
+    getWinner();
 }
 
-function game() {
-    userScore = 0;
-    comScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let userInput;
-        do{
-           userInput = getUserInput(); 
-        } while (userInput == -1); // Checks if userInput is in list; indexOf returns -1 if it is not in the list
-        let comInput = Math.floor(Math.random() * 3); // Gets a random number for comInput from 0 to 2
-        console.log(comInput);
-        console.log(getWinner(userInput, comInput));
-        console.log(`User: ${userScore} Computer: ${comScore}`);
+function getWinner() {
+    if (userScore === 5) {
+        winner.textContent = 'You Won!';
+    } else if (comScore === 5) {
+        winner.textContent = 'You Lost!';
     }
-
-    if (comScore > userScore) {
-        console.log('You Lose!');
-    } else {
-        console.log('You Win!');
-    }
-
-    return;
+    buttons.forEach((button) => {
+        button.removeEventListener('click', (e) => {startRound(e);});
+    });
 }
-
-game();
